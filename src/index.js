ab4ffcf79ccb3bc3b1b12d5c1a788220622f4114
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-05 10:24:18 -0500

    Listen for up click

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-'use strict';
+const upBtn = document.querySelector('.btn--up');
+
+function upClick() {
+  // Do something we can know this works?
+  console.log('YAAZZZZ');
+}
+
+upBtn.addEventListener('click', upClick);
