493b7d96d7ce122e6b345ee2241257b3047f4c36
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-05 11:12:07 -0500

    Connect down button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export default function (app) {
   const upBtn = app.querySelector('.btn--up');
+  const downBtn = app.querySelector('.btn--down');
   const countElement = app.querySelector('.counter__count');
 
   function upClick() {
@@ -9,5 +10,13 @@ export default function (app) {
     countElement.innerText = oldNumber + 1;
   }
 
+  function downClick() {
+    // Do something we can know this works?
+    const oldNumber = parseInt(countElement.innerText, 10);
+
+    countElement.innerText = oldNumber - 1;
+  }
+
   upBtn.addEventListener('click', upClick);
+  downBtn.addEventListener('click', downClick);
 }
