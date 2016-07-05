9116a189f9e02920228f9dac6766dc1028a0f998
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-05 12:12:13 -0500

    Show name of character

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export default function (app) {
   const upBtn = app.querySelector('.btn--up');
   const downBtn = app.querySelector('.btn--down');
   const countElement = app.querySelector('.counter__count');
+  const charInfo = app.querySelector('.character-info');
 
   let number = 1;
 
@@ -25,4 +26,18 @@ export default function (app) {
 
   upBtn.addEventListener('click', upClick);
   downBtn.addEventListener('click', downClick);
+
+  // Go ask for info from this URL
+  fetch('https://swapi.co/api/people/1')
+    // Wait for a response back and turn the string of JSON into an actual JS object
+    .then((res) => res.json())
+    // Once we have a JS object to work with, here's what we're gonna do with it
+    .then((data) => {
+      // how do i know what data is???
+      console.log(data);
+      charInfo.innerHTML = `
+        <h2>${data.name}</h2>
+        <ul className="movie-list"></ul>
+      `;
+    });
 }
